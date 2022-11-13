5aef8df4c1043db36ee789d38574174c403b3c84
Erica Zhang <ericazhang@MacBook-Pro-326.local>
2022-11-13 13:59:17 -0800

    new change

--- overweight_prevalence_dataset.R
+++ overweight_prevalence_dataset.R
@@ -5,17 +5,17 @@ overweight_prevalence1 <- overweight_prevalence %>%
   slice(13:15, 17, 89:102) %>%
   select(X, X.12, X.17, X.22, X.27, X.32, X.37, X.42, X.47, X.52, X.57, X.62) %>%
   rename( "Region" = X,
-  "2010 (overweight prevalence)" = X.12, 
-  "2011 (overweight prevalence)" = X.17, 
-  "2012 (overweight prevalence)" = X.22,
-  "2013 (overweight prevalence)" = X.27, 
-  "2014 (overweight prevalence)" = X.32, 
-  "2015 (overweight prevalence)" = X.37,   
-  "2016 (overweight prevalence)" = X.42, 
-  "2017 (overweight prevalence)" = X.47,
-  "2018 (overweight prevalence)" = X.52,
-  "2019 (overweight prevalence)" = X.57,
-  "2020 (overweight prevalence)" = X.62) %>% 
+  "2010_overweight_prevalence" = X.12, 
+  "2011_overweight_prevalence" = X.17, 
+  "2012_overweight_prevalence" = X.22,
+  "2013_overweight_prevalence)" = X.27, 
+  "2014_overweight_prevalence" = X.32, 
+  "2015_overweight_prevalence" = X.37,   
+  "2016_overweight_prevalence" = X.42, 
+  "2017_overweight_prevalence" = X.47,
+  "2018_overweight_prevalence" = X.52,
+  "2019_overweight_prevalence" = X.57,
+  "2020_overweight_prevalence" = X.62) %>% 
   slice(4:18)
 
   write.csv(overweight_prevalence1, "/Users/ericazhang/Documents/info201/project-group-1-section-ab/overweight_prevalence.csv", row.names = FALSE)
